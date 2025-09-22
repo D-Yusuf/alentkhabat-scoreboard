e12d80a4d101726576790043bbd3f6bcf7efe7ed
[dyad] <mr.alshmry@gmail.com>
2025-09-22 12:58:47 +0300

    [dyad] wrote 2 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import BottomNav from './BottomNav';
 const Layout = () => {
   return (
     <div className="h-screen bg-background text-foreground">
-      <main className="p-4 container mx-auto max-w-md h-full pb-20 box-border flex flex-col">
+      <main className="p-4 container mx-auto max-w-md h-full pb-20 box-border flex flex-col bg-background">
         <Outlet />
       </main>
       <BottomNav />
