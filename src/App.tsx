7f8e125ba0caa2f43f02be5cde618489a9a1c9d9
[dyad] <mr.alshmry@gmail.com>
2025-09-22 11:44:22 +0300

    Reverted all changes back to version c957344dc1b4518e8a665c89eb40e65fc7216474

--- src/App.tsx
+++ src/App.tsx
@@ -3,53 +3,24 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Teams from "./pages/Teams";
-import Players from "./pages/Players";
-import ScoreList from "./pages/ScoreList";
-import Settings from "./pages/Settings";
+import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { ScoreProvider } from "@/context/ScoreContext";
-import { useTranslation } from 'react-i18next';
-import { useEffect } from 'react';
-import { ThemeProvider } from "@/components/ThemeProvider"; // Import ThemeProvider
 
 const queryClient = new QueryClient();
 
-const AppContent = () => {
-  const { i18n } = useTranslation();
-
-  useEffect(() => {
-    document.documentElement.lang = i18n.language;
-    document.documentElement.dir = i18n.dir(i18n.language);
-  }, [i18n, i18n.language]);
-
-  return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Teams />} />
-        <Route path="/players" element={<Players />} />
-        <Route path="/score-list" element={<ScoreList />} />
-        <Route path="/settings" element={<Settings />} />
-      </Route>
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
-
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
-      <TooltipProvider>
-        <ScoreProvider>
-          <Toaster />
-          <Sonner />
-          <BrowserRouter>
-            <AppContent />
-          </BrowserRouter>
-        </ScoreProvider>
-      </TooltipProvider>
-    </ThemeProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
   </QueryClientProvider>
 );
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
-import "./i18n";
-import React from "react";
 
-createRoot(document.getElementById("root")!).render(
-  <React.Suspense fallback="loading...">
-    <App />
-  </React.Suspense>
-);
+createRoot(document.getElementById("root")!).render(<App />);
