e6796c77ce46d6ca16789bb8eb031099badb2708
YAlnasiri <alnasiriyusuf@gmail.com>
2025-10-08 11:45:06 +0300

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(() => ({
   server: {
